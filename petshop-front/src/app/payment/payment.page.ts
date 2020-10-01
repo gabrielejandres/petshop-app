32671e3c979735650949a9b251bbfc99483e96fd
LucianaVit <luciana.vbnd@gmail.com>
2020-09-30 23:31:42 -0300

    +campos para dados do cartão

--- petshop-front/src/app/payment/payment.page.ts
+++ petshop-front/src/app/payment/payment.page.ts
@@ -10,12 +10,21 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class PaymentPage implements OnInit {
 
     paymentForm: FormGroup;
+    cvv: number;
+    validity: number;
+    cpf: number;
+    cpfOrCnpj: number;
 
     constructor(public formbuilder:FormBuilder) {
       this.paymentForm = this.formbuilder.group({
           address:[null, [Validators.required, Validators.minLength(10)]],
           formOfPayment:[null, [Validators.required]],
           cpf:[null, [Validators.minLength(11)]],
+          numberCard: [null, [Validators.required, Validators.minLength(14)]],
+          validity: [null, [Validators.required, Validators.minLength(4), Validators.maxLength(4)]],
+          cvv: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(3)]],
+          name: [null, [Validators.required, Validators.minLength(2)]],
+          cpfOrCnpj: [null, [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
       })
     }
 
