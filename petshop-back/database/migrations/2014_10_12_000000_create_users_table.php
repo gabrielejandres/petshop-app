0ec2a041ac3f6e47e22ee815f5c8347f54826a80
andradenathan <nathan.nhdsk@gmail.com>
2020-09-30 23:12:46 -0300

    + Atributos da tabela do usuário

--- petshop-back/database/migrations/2014_10_12_000000_create_users_table.php
+++ petshop-back/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('doctype');
+            $table->bigInteger('docvalue');
+            $table->string('telephone');
             $table->rememberToken();
             $table->timestamps();
         });
