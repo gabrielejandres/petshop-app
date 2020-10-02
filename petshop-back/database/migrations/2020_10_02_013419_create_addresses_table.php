6f23fc691a886ce1baf443a743ea97e8311ba08b
Gabriel Cunha <gabrielrcunha15@gmail.com>
2020-10-02 20:05:18 -0300

    + migration de Address

---
+++ petshop-back/database/migrations/2020_10_02_013419_create_addresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->String('street');
+            $table->String('number');
+            $table->String('district');
+            $table->String('city');
+            $table->String('state');
+            $table->String('country')->default('BRA');
+            $table->String('postalCode');
+            $table->String('complement')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
